252c54c56d7a948ca7ea88006b545b5b8cd90493
Pham Thuc Hung <hungpt169@gmail.com>
2019-09-08 12:10:08 +0900

    2nd for 20 epochs

--- tf_based/train_all.py
+++ tf_based/train_all.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     input_path = '../data/processed data/'
     output_path = '../output/1/'
 
-    dimension_list = [25, 75, 125, 175, 225, 275]
-    # dimension_list = [50, 100, 150, 200, 250, 300]
+    # dimension_list = [25, 75, 125, 175, 225, 275]
+    dimension_list = [50, 100, 150, 200, 250, 300]
 
     for dimension in dimension_list:
         skip_gram = SkipGram(input_path, output_path, n_embedding=dimension)
