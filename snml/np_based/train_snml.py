03ad635d9a167b8c8ea59270ffc5bb5d0797adc0
Pham Thuc Hung <hungpt169@gmail.com>
2019-09-09 19:08:25 +0900

    ready to run on sevrer

--- snml/np_based/train_snml.py
+++ snml/np_based/train_snml.py
@@ -1,10 +1,12 @@
 from snml.np_based.model import Model
+import random
 import time
 import numpy as np
 import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--dim', default=50, type=int)
     parser.add_argument('--snml_train_file', default='../../../data/processed data/scope.csv', type=str)
     parser.add_argument('--output_path', default='models/100dim/output/', type=str)
     parser.add_argument('--context_distribution_file', default='../context_distribution.pkl', type=str)
@@ -12,47 +14,25 @@ if __name__ == "__main__":
 
     # read snml train file
     data = np.genfromtxt(args.snml_train_file, delimiter=',').astype(int)
+    data = data[:100]
 
-    # 50 dim
-    model = Model('../models/50dim/', args.context_distribution_file, n_context_sample=6000)
-    total_length = 0
-    for i in range(5):
-        w = data[i][0]
-        c = data[i][1]
+    # training
+    model_path = '../models/{}dim/'.format(args.dim)
+    model = Model(model_path, args.context_distribution_file, n_context_sample=600)
+    leng1 = 0
+    iteration = 0
+    start = time.time()
+    for datum in data:
+        w = datum[0]
+        c = datum[1]
 
-        snml_length = model.snml_length_sampling(w, c)
-        total_length += snml_length
-    print(total_length)
+        iteration += 1
+        if iteration % 10 == 0:
+            end = time.time()
+            print("Iteration: {}, ".format(iteration),
+                  "{:.4f} sec".format(end - start))
+            start = time.time()
 
-    # 100 dim
-    model = Model('../models/100dim/', args.context_distribution_file, n_context_sample=6000)
-    total_length = 0
-    for i in range(5):
-        w = data[i][0]
-        c = data[i][1]
-
-        snml_length = model.snml_length_sampling(w, c)
-        total_length += snml_length
-    print(total_length)
-
-    # 150 dim
-    model = Model('../models/150dim/', args.context_distribution_file, n_context_sample=6000)
-    total_length = 0
-    for i in range(5):
-        w = data[i][0]
-        c = data[i][1]
-
-        snml_length = model.snml_length_sampling(w, c)
-        total_length += snml_length
-    print(total_length)
-
-    # 200 dim
-    model = Model('../models/200dim/', args.context_distribution_file, n_context_sample=6000)
-    total_length = 0
-    for i in range(5):
-        w = data[i][0]
-        c = data[i][1]
-
-        snml_length = model.snml_length_sampling(w, c)
-        total_length += snml_length
-    print(total_length)
+        snml_length, prob_sum = model.snml_length_sampling(w, c)
+        leng1 += snml_length
+    print(leng1)
