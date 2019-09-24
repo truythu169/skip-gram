e1e1605792a3f0481c65861c374835529de45086
Pham Thuc Hung <hungpt169@gmail.com>
2019-09-24 16:19:17 +0900

    add data

--- snml/np_based/train_snml.py
+++ snml/np_based/train_snml.py
@@ -5,7 +5,7 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model', default='../models/31epochs/100dim/', type=str)
+    parser.add_argument('--model', default='../models/31epochs/50dim/', type=str)
     parser.add_argument('--snml_train_file', default='../../../data/processed data/scope.csv', type=str)
     parser.add_argument('--scope', default=100, type=int)
     args = parser.parse_args()
